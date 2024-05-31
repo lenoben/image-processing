5cc63c6982905649c17f4f6d6508217eab0d3e43
lenoben <lenoben002@gmail.com>
2024-05-31 18:21:19 +0300

    Binary gray load impl

--- src/BinaryGray.cpp
+++ src/BinaryGray.cpp
@@ -1,14 +1,64 @@
 #include "BinaryGray.hpp"
 
-BinaryGrayImage::BinaryGrayImage(const BinaryGrayImage &other) : matrix(other.matrix), width(other.width), height(other.height){};
+BinaryGrayImage::BinaryGrayImage(const BinaryGrayImage &other) : matrix(other.matrix), width(other.width), height(other.height), th(other.th){};
 
 BinaryGrayImage::BinaryGrayImage(int width, int height) : height(height), width(width), th(128.0), matrix(IntMatrix(height, IntArray(width))){};
+
+BinaryGrayImage::BinaryGrayImage(int width, int height, float tth) : height(height), width(width), th(tth), matrix(IntMatrix(height, IntArray(width))){};
 
 BinaryGrayImage::BinaryGrayImage(IntMatrix &tmatrix)
 {
     matrix = tmatrix;
     width = tmatrix[0].size();
     height = tmatrix.size();
+}
+
+void BinaryGrayImage::Load(GrayscaleImage &img, bool adaptativeThreshold, float manualThreshold)
+{
+    th = manualThreshold;
+    float prevth = th;
+    width = img.GetWidth();
+    height = img.GetHeight();
+
+    matrix = IntMatrix(height, IntArray(width));
+
+    double sum_true, sum_false, count_true, count_false;
+
+    if (adaptativeThreshold)
+    {
+        do
+        {
+            prevth = th;
+            sum_true = 0;
+            sum_false = 0;
+            count_true = 0;
+            count_false = 0;
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (img(x, y) > th)
+                    {
+                        sum_true += img(x, y);
+                        count_true++;
+                    }
+                    else
+                    {
+                        sum_false += img(x, y);
+                        count_false++;
+                    }
+                }
+            }
+            th = float(sum_true / count_true + sum_false / count_false) / 2;
+        } while (th != prevth);
+    }
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            (*this)(x, y) = img(x, y) > th ? 1 : 0;
+        }
+    }
 }
 
 int BinaryGrayImage::operator()(int x_width, int y_height) const
