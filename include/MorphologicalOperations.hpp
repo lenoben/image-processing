e776ffd8a8a0d69a14905bc5b08f39ae7102314d
lenoben <lenoben002@gmail.com>
2024-08-10 10:41:43 +0300

    print matrix func

---
+++ include/MorphologicalOperations.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "BinaryGray.hpp"
+
+void printMatrix(IntMatrix &Tmatrix);
--- src/MorphologicalOperations.cpp
+++ src/MorphologicalOperations.cpp
@@ -1,1 +1,16 @@
 #include "MorphologicalOperations.hpp"
+
+void printMatrix(IntMatrix &Tmatrix)
+{
+    for (const auto &row : Tmatrix)
+    {
+        for (const auto &elem : row)
+        {
+            // std::cout << (elem ? "X" : " ") << " ";
+            std::cout << (elem ? "[X]" : "[ ]") << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl;
+}
