14c3fb75adadd0f7c33c9c4e5948820290536218
lenoben <lenoben002@gmail.com>
2024-11-02 13:26:12 +0300

    feat(connected.component): unionFind init

--- include/ConnectedComponents.hpp
+++ include/ConnectedComponents.hpp
@@ -1,6 +1,16 @@
 #pragma once
 #include "BinaryGray.hpp"
 
+class UnionFind
+{
+public:
+    UnionFind(int n);
+
+private:
+    std::vector<int> parent;
+    std::vector<int> rank;
+};
+
 bool checkboundary(int x, int xmax);
 
 int checkinside(pixelcoordinate pc, std::vector<label_und_pixel> &lup);
--- src/ConnectedComponents.cpp
+++ src/ConnectedComponents.cpp
@@ -1,5 +1,13 @@
 #include "ConnectedComponents.hpp"
 
+UnionFind::UnionFind(int n) : parent(n), rank(n, 0)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        parent[i] = i;
+    }
+}
+
 bool checkboundary(int x, int xmax)
 {
     return x >= 0 && x < xmax;
